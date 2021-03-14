3a966c74dfb207daeb5828117c414de98d3a8ed4
KentaroSawada654 <74202564+KentaroSawada654@users.noreply.github.com>
2021-03-14 22:52:03 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("App");
+  const [text, setText] = useState(0);
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClickCountUp = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button>表示</button>
+      <ChildArea />
+    </div>
+  );
+}
